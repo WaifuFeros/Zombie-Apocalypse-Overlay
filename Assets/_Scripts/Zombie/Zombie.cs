eb1262d0114621b6b278381e2901dd79980aa6ff
WaifuFeros <payet.pep@gmail.com>
2024-11-09 17:32:11 +0100

    Zombie Take damage and die

--- Assets/_Scripts/Zombie/Zombie.cs
+++ Assets/_Scripts/Zombie/Zombie.cs
@@ -1,10 +1,18 @@
-using System;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    private static List<Zombie> All = new List<Zombie>();
+
     [field:SerializeField] public int hp { get; private set; } = 10;
     [SerializeField] private float speed = 5;
+
+    private void Start()
+    {
+        All.Add(this);
+    }
 
     private void Update()
     {
@@ -20,6 +28,15 @@ public class Zombie : MonoBehaviour
 
     private void Die()
     {
+        All.Remove(this);
         Destroy(gameObject);
+    }
+
+    public static void DamageZombie(int damage)
+    {
+        if (All.Count <= 0)
+            return;
+
+        All[0].TakeDamage(damage);
     }
 }
