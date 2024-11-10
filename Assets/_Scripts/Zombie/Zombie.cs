5ba24205802a431512adf5bd130252ccd774d92b
WaifuFeros <payet.pep@gmail.com>
2024-11-10 19:01:05 +0100

    Zombie random zombie speed + kill zone v1

--- Assets/_Scripts/Zombie/Zombie.cs
+++ Assets/_Scripts/Zombie/Zombie.cs
@@ -6,30 +6,51 @@ public class Zombie : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
 
-    [field:SerializeField] public int hp { get; private set; } = 10;
-    [SerializeField] private float speed = 5;
+    [field:SerializeField] public int HP { get; private set; } = 10;
+    [SerializeField] private Vector2 _speedRange = new Vector2(0.8f, 1.2f);
+    [SerializeField] private float _speedMultiplier = 0.1f;
+    [SerializeField] private Vector2 _killzone = new Vector2(-9.5f, 9.5f);
+
+    private float _speed = 0.1f;
 
     private void Start()
     {
+        _speed = Random.Range(_speedRange.x, _speedRange.y) * _speedMultiplier;
         ZombieWaveManager.AddZombie(this);
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        if (transform.position.x < _killzone.x || transform.position.x > _killzone.y)
+            Die();
     }
 
     public void TakeDamage(int damage)
     {
-        hp -= damage;
+        HP -= damage;
         _animator.SetTrigger("Hit");
-        if (hp <= 0)
+        if (HP <= 0)
             Die();
     }
 
     private void Die()
     {
+        Despawn();
+    }
+
+    private void Despawn()
+    {
         ZombieWaveManager.RemoveZombie(this);
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 center = Vector3.zero;
+        center.x += (_killzone.x + _killzone.y) / 2;
+        center.y = transform.position.y;
+        float size = _killzone.y - _killzone.x;
+        Gizmos.DrawWireCube(center, new Vector3(size, 1, 0));
     }
 }
